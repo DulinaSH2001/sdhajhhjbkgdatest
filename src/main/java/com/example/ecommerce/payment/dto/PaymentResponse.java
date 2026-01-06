9c0bd5af6c34f1dce3019d41827e99a3eeda98fe
Dulina Hejitha <128177354+DulinaSH2001@users.noreply.github.com>
2026-01-06 22:40:55 +0530

    Add src/main/java/com/example/ecommerce/payment/dto/PaymentResponse.java

---
+++ src/main/java/com/example/ecommerce/payment/dto/PaymentResponse.java
@@ -0,0 +1,22 @@
+package com.example.ecommerce.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponse {
+    private Long id;
+    private Long orderId;
+    private String paymentMethod;
+    private BigDecimal amount;
+    private LocalDateTime paymentDate;
+    private String status;
+}
